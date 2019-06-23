5cdc4741cdf08a2c556edcfbe05ed00e515f9312
Jeffrey Fleer <Jeff@as-MacBook-Pro.router>
2019-06-23 12:19:33 -0600

    created skeleton

---
+++ app/data/friends.js
@@ -0,0 +1,32 @@
+var friends = [
+    {
+        name: "Andrea Dovisioso",
+        photo: "https://photobest1.com/wp-content/uploads/2018/04/MotoGP-Andrea-Dovizioso-wallpaper-hd-10.jpg",
+        scores: [5,1,4,4,5,1,2,5,4,1]
+    },
+    {
+        name: "Jorge Lorenzo",
+        photo: "https://cdn-1.motorsport.com/images/amp/2wBJob30/s6/jorge-lorenzo-repsol-honda-tea.jpg",
+        scores:[4,2,3,1,5,5,3,1,3,2]
+    },
+    {
+        name: "Marc Marquez",
+        photo: "https://wallpapercave.com/wp/wp1836053.jpg",
+        scores:[5,5,5,4,1,3,2,1,4,3]
+    },
+    {
+        name: "Valentino Rossi",
+        photo: "http://backgrounds4k.net/wp-content/uploads/2016/04/Valentino-Rossi-background-3840x2160.jpg",
+        scores: [5,5,5,5,5,5,5,5,5,5]
+    },
+    {
+        name: "Marco Simoncelli",
+        photo: "http://images6.fanpop.com/image/photos/32500000/-3-Marco-Simoncelli-valentino-rossi-32561453-800-593.jpg",
+        scores: [3,3,2,4,2,4,4,5,1,1]
+    },
+    {
+        name: "Casey Stoner",
+        photo: "https://ultimatemotorcycling.com/wp-content/uploads/2018/11/2018-casey-stoner-and-ducati-part-ways.jpg",
+        scores: [4,4,1,2,1,3,1,4,5,2]
+    }
+]
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,24 @@
+// *********************************************************************************
+// htmlRoutes.js - this file offers a set of routes for sending users to the various html pages
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+var path = require("path");
+
+// Routes
+// =============================================================
+module.exports = function (app) {
+
+    // Each of the below routes just handles the HTML page that the user gets sent to.
+
+    // Route loads home.html
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+
+    // Route loads survey.html
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+};
