dff6da7ce141940a8330bfebf55b12931315450a
Jeffrey Fleer <Jeff@as-MacBook-Pro.local>
2019-06-23 16:43:01 -0600

    Fixed <select> bug causing options to not display

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,12 @@
-app.get("/api/friends", function(req, res){
+module.exports = function(app){
+
+    app.get("/api/friends", function(req, res){
     res.json(friends);
 });
 
 app.post("/api/friends", function(req, res){
-    
+    var newUser = req.body;
+    console.log(newUser);
+    friends.push(newUser)
 })
+}
--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var app     = express();
 
 var PORT = 8080;
 
+require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
+
 app.listen(PORT, function(){
     console.log("App listening on PORT ", PORT)
 })
